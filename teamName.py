2c1b2ecb9f725320b9c2db0d311ac00cf2c77d35
Quy <20696858+KhanhQuy@users.noreply.github.com>
2023-08-15 21:41:15 +1000

    Apply pair trading.
    Apply pair trading for code.

--- teamName.py
+++ teamName.py
@@ -1,90 +1,39 @@
-#!/usr/bin/env python
-                        ###############
-                        #Apply mean reversion strategy
-# import numpy as np
-
-# nInst = 50
-# currentPos = np.zeros(nInst)
-
-# def mean_reversion_strategy(price_history, window=50, z_threshold=1.0):
-#     mean = price_history[-window:].mean()
-#     std = price_history[-window:].std()
-#     z_score = (price_history[-1] - mean) / std
-
-#     if z_score > z_threshold:
-#         return -1  # Short position
-#     elif z_score < -z_threshold:
-#         return 1   # Long position
-#     else:
-#         return 0   # No position
-    
-
-# def getMyPosition(prcSoFar):
-#     global currentPos
-#     (nins, nt) = prcSoFar.shape
-
-#     if nt < 51:  # Need at least 51 data points to compute 50-day mean reversion
-#         return np.zeros(nins)
-    
-#     for inst in range(nins):
-#         price_history = prcSoFar[inst, :]
-#         currentPos[inst] = mean_reversion_strategy(price_history)
-        
-#     return currentPos
-
-
-                        
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
 
 nInst = 50
 currentPos = np.zeros(nInst)
-lookback = 50
 
-def generate_features(price_history):
-    returns = np.diff(price_history)
-    lagged_returns = np.roll(returns, 1)
-    lagged_returns[0] = 0
-    return np.column_stack((lagged_returns, returns))
+def zscore(series):
+    return (series - series.mean()) / np.std(series)
 
-def mean_reversion_strategy(price_history, window=lookback, z_threshold=1.0):
-    mean = price_history[-window:].mean()
-    std = price_history[-window:].std()
-    z_score = (price_history[-1] - mean) / std
+def pairs_trading_strategy(prices_A, prices_B, window=50, z_threshold=1.0):
+    # Calculate spread and its z-score
+    spread = prices_A - prices_B
+    z_scores = zscore(spread[-window:])
+    current_z_score = z_scores[-1]
 
-    if z_score > z_threshold:
-        return -1  # Short position
-    elif z_score < -z_threshold:
-        return 1   # Long position
+    # Trading logic
+    if current_z_score > z_threshold:
+        return -1, 1  # short A, long B
+    elif current_z_score < -z_threshold:
+        return 1, -1  # long A, short B
     else:
-        return 0   # No position
+        return 0, 0  # No position
 
 def getMyPosition(prcSoFar):
     global currentPos
     (nins, nt) = prcSoFar.shape
 
-    if nt < 51:  # Need at least 51 data points to compute 50-day mean reversion and generate features
+    if nt < 51:  # Need at least 51 data points to compute 50-day mean reversion
         return np.zeros(nins)
     
-    for inst in range(nins):
-        price_history = prcSoFar[inst, :]
-        decision = mean_reversion_strategy(price_history)
-        
-        if decision == 0:
-            X = generate_features(price_history)
-            y = np.roll(price_history, -1)[:-1]  # shifted price history as target
-            
-            model = RandomForestRegressor(n_estimators=100)
-            model.fit(X[:-1], y[:-1])  # Train using all data points except the last one
-            
-            forecast = model.predict([X[-1]])
-            if forecast > price_history[-1]:  # If forecasted price is higher than the last observed price
-                currentPos[inst] = 1
-            elif forecast < price_history[-1]:  # If forecasted price is lower than the last observed price
-                currentPos[inst] = -1
-            else:
-                currentPos[inst] = 0
-        else:
-            currentPos[inst] = decision
+    # Assume asset 0 and asset 1 are our correlated pair (this is just an example)
+    position_A, position_B = pairs_trading_strategy(prcSoFar[0], prcSoFar[1])
+    currentPos[0] = position_A
+    currentPos[1] = position_B
+
+    # Rest of the assets remain neutral
+    for inst in range(2, nins):
+        currentPos[inst] = 0
         
     return currentPos
